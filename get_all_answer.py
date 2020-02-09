08f00877763e2ba6a8d6f7a4cd5098cf69ffe75b
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-09 17:56:24 +0800

    [ZBook] Format answer api

--- get_all_answer.py
+++ get_all_answer.py
@@ -23,16 +23,9 @@ def get_answers(question_id):
     # 我们获取数据的URL格式是什么样呢？
     # https://www.zhihu.com/api/v4/questions/39162814/answers?
     # sort_by=default&include=data[*].is_normal,content&limit=5&offset=0
-    url = "https://www.zhihu.com/api/v4/questions/" \
-          + question_id + "/answers" \
-          "?sort_by=default&include=data[*].is_normal,voteup_count,content" \
-          "&limit=" + str(limit) + "&offset=" + str(offset)
+    url = "https://www.zhihu.com/api/v4/questions/{0}/answers?sort_by={1}&include=data[*].is_normal,voteup_count,content&limit={2}&offset={3}"
+    url = url.format(question_id, sort_by, limit, offset)
 
-    '''
-    "https://www.zhihu.com/api/v4/questions/" \
-          + question_id + "/answers" \ "?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&" \
-       "&limit=" + str(limit) + "&offset=" + str(offset) + "&platform=desktop&sort_by=default"
-    '''
 
     '''
     折叠的回答
