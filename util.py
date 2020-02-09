69597ba278b3c05599dc0bcbc6b29a6b4bb1ef27
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-09 22:00:45 +0800

    [ZBook] Adjust function position

--- util.py
+++ util.py
@@ -8,106 +8,6 @@ from datetime import datetime
 from ebooklib import epub
 
 
-def get_author_info_content(author):
-    author_url = "https://www.zhihu.com/people/hydfox" + author["url_token"]
-
-    author_info_content = """
-    <div class="AuthorInfo-content">
-     <div class="AuthorInfo-head">
-      <span class="UserLink AuthorInfo-name">
-       <div class="Popover">
-        <div id="Popover222-toggle" aria-haspopup="true" aria-expanded="false" aria-owns="Popover222-content">
-         作者：<a class="UserLink-link" data-za-detail-view-element_name="User" target="_blank" href="{0}">{1}</a>
-        </div>
-       </div>
-      </span>
-     </div>
-     <div class="AuthorInfo-detail">
-      <div class="AuthorInfo-badge">
-       <div class="AuthorInfo-badgeText">
-        签名：{2}
-       </div>
-      </div>
-     </div>
-    </div>
-    <br/>
-    """.format(author_url, author["name"], author["headline"])
-
-    return author_info_content
-
-
-def download_image(image_url, image_path):
-    """
-    1. 如果图片没有下载过，下载
-    2. 如果图片下载过，什么都不做
-    """
-    if not os.path.isfile(image_path):
-        with open(image_path, "wb") as handler:
-            img_data = requests.get(image_url).content
-            handler.write(img_data)
-            time.sleep(3)
-    else:
-        pass
-
-
-def parse_answer_content(answer, answer_number):
-    """
-    格式化答案：
-    1. 去除答案首部和尾部的换行
-    2. 处理图片
-    """
-
-    """
-    得到的图片是如下这样表示的
-    '<figure><noscript><img src="https://pic2.zhimg.com/50/dabb778580858a9d05e837e29b54e445_hd.jpg" data-rawwidth="155" data-rawheight="155" class="content_image" width="155"/></noscript><img src="data:image/svg+xml;utf8,&lt;svg xmlns=&#39;http://www.w3.org/2000/svg&#39; width=&#39;155&#39; height=&#39;155&#39;&gt;&lt;/svg&gt;" data-rawwidth="155" data-rawheight="155" class="content_image lazy" width="155" data-actualsrc="https://pic2.zhimg.com/50/dabb778580858a9d05e837e29b54e445_hd.jpg"/></figure>
-    <noscript> 标签里的可以直接用
-    后面的那个不行，因此去掉后面的，留下 <noscript> 里的
-    """
-    answer_content = answer["content"]
-    answer_id = answer["id"]
-    answer_content = answer_content.replace("</noscript>", "")
-    answer_content = answer_content.replace("<noscript>", "")
-
-    image_regex = r"<img.*?/>"
-    image_tag_list = re.findall(image_regex, answer_content, re.S | re.M)
-
-    for i in range(len(image_tag_list)):
-        if i % 2 == 0:
-            pass
-        else:
-            answer_content = answer_content.replace(image_tag_list[i], "")
-
-    # 现在 answer 里的图片都是 <img src="https://pic2.zhimg.com/50/dabb778580858a9d05e837e29b54e445_hd.jpg" data-rawwidth="155" data-rawheight="155" class="content_image" width="155"/> 这样了
-    # 把这些图片下载到本地
-    image_url_regex = r"<img src=\"(.*?)\""
-    image_url_list = re.findall(image_url_regex, answer_content, re.S | re.M)
-
-    image_name_list = []
-    dir_path = "./images/"
-    for i in range(len(image_url_list)):
-        image_url = image_url_list[i]
-        # image_url.split("/")[-1]
-        image_name = "{}-{}-{}.jpg".format(answer_id, answer_number, i)
-
-        download_image(image_url, dir_path+image_name)
-
-        answer_content = answer_content.replace(image_url, image_name)
-        image_name_list.append(image_name)
-
-    return answer_content, dir_path, image_name_list
-
-
-def get_time_content(answer):
-    created_time = int(answer["created_time"])
-    time_content = "发布于：%s" % datetime.utcfromtimestamp(created_time).strftime("%Y-%m-%d %H:%M:%S")
-    updated_time = int(answer["updated_time"])
-
-    if updated_time != created_time:
-        time_content += "<br/>修改于：%s" % datetime.utcfromtimestamp(updated_time).strftime("%Y-%m-%d %H:%M:%S")
-
-    return time_content
-
-
 def customize_create_toc(chapter_list):
     def chunks(l, n):
         """Yield successive n-sized chunks from l."""
@@ -268,3 +168,103 @@ def create_chapter_from_answer(book, answer, cur_answer_count):
         book.add_item(image)
 
     return chapter, len(image_name_list)
+
+
+def get_author_info_content(author):
+    author_url = "https://www.zhihu.com/people/hydfox" + author["url_token"]
+
+    author_info_content = """
+    <div class="AuthorInfo-content">
+     <div class="AuthorInfo-head">
+      <span class="UserLink AuthorInfo-name">
+       <div class="Popover">
+        <div id="Popover222-toggle" aria-haspopup="true" aria-expanded="false" aria-owns="Popover222-content">
+         作者：<a class="UserLink-link" data-za-detail-view-element_name="User" target="_blank" href="{0}">{1}</a>
+        </div>
+       </div>
+      </span>
+     </div>
+     <div class="AuthorInfo-detail">
+      <div class="AuthorInfo-badge">
+       <div class="AuthorInfo-badgeText">
+        签名：{2}
+       </div>
+      </div>
+     </div>
+    </div>
+    <br/>
+    """.format(author_url, author["name"], author["headline"])
+
+    return author_info_content
+
+
+def download_image(image_url, image_path):
+    """
+    1. 如果图片没有下载过，下载
+    2. 如果图片下载过，什么都不做
+    """
+    if not os.path.isfile(image_path):
+        with open(image_path, "wb") as handler:
+            img_data = requests.get(image_url).content
+            handler.write(img_data)
+            time.sleep(3)
+    else:
+        pass
+
+
+def parse_answer_content(answer, answer_number):
+    """
+    格式化答案：
+    1. 去除答案首部和尾部的换行
+    2. 处理图片
+    """
+
+    """
+    得到的图片是如下这样表示的
+    '<figure><noscript><img src="https://pic2.zhimg.com/50/dabb778580858a9d05e837e29b54e445_hd.jpg" data-rawwidth="155" data-rawheight="155" class="content_image" width="155"/></noscript><img src="data:image/svg+xml;utf8,&lt;svg xmlns=&#39;http://www.w3.org/2000/svg&#39; width=&#39;155&#39; height=&#39;155&#39;&gt;&lt;/svg&gt;" data-rawwidth="155" data-rawheight="155" class="content_image lazy" width="155" data-actualsrc="https://pic2.zhimg.com/50/dabb778580858a9d05e837e29b54e445_hd.jpg"/></figure>
+    <noscript> 标签里的可以直接用
+    后面的那个不行，因此去掉后面的，留下 <noscript> 里的
+    """
+    answer_content = answer["content"]
+    answer_id = answer["id"]
+    answer_content = answer_content.replace("</noscript>", "")
+    answer_content = answer_content.replace("<noscript>", "")
+
+    image_regex = r"<img.*?/>"
+    image_tag_list = re.findall(image_regex, answer_content, re.S | re.M)
+
+    for i in range(len(image_tag_list)):
+        if i % 2 == 0:
+            pass
+        else:
+            answer_content = answer_content.replace(image_tag_list[i], "")
+
+    # 现在 answer 里的图片都是 <img src="https://pic2.zhimg.com/50/dabb778580858a9d05e837e29b54e445_hd.jpg" data-rawwidth="155" data-rawheight="155" class="content_image" width="155"/> 这样了
+    # 把这些图片下载到本地
+    image_url_regex = r"<img src=\"(.*?)\""
+    image_url_list = re.findall(image_url_regex, answer_content, re.S | re.M)
+
+    image_name_list = []
+    dir_path = "./images/"
+    for i in range(len(image_url_list)):
+        image_url = image_url_list[i]
+        # image_url.split("/")[-1]
+        image_name = "{}-{}-{}.jpg".format(answer_id, answer_number, i)
+
+        download_image(image_url, dir_path+image_name)
+
+        answer_content = answer_content.replace(image_url, image_name)
+        image_name_list.append(image_name)
+
+    return answer_content, dir_path, image_name_list
+
+
+def get_time_content(answer):
+    created_time = int(answer["created_time"])
+    time_content = "发布于：%s" % datetime.utcfromtimestamp(created_time).strftime("%Y-%m-%d %H:%M:%S")
+    updated_time = int(answer["updated_time"])
+
+    if updated_time != created_time:
+        time_content += "<br/>修改于：%s" % datetime.utcfromtimestamp(updated_time).strftime("%Y-%m-%d %H:%M:%S")
+
+    return time_content
